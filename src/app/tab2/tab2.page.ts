0fe56566a40ad629892e6be5913a804904906fb1
Diemisom Carlos Romano de Melo <dcarlospa@gmail.com>
2021-09-03 21:28:46 -0300

    Testes gerais de componentes do Ionic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController, PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  alunos = ['George', 'Laércio', 'Lucas', 'David'];
+  constructor(
+    public popoverController: PopoverController,
+    public loadingController: LoadingController
+    ) {
+
+  }
+
+  acao(nomeBotao){
+    console.log(nomeBotao);
+  }
+
+  removeAluno(aluno){
+    this.alunos.splice(this.alunos.indexOf(aluno), 1);
+  }
+
+  addAluno(){
+    this.alunos.push('George '+this.alunos.length);
+
+  }
+
+  async carregar() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Aguarde...',
+      duration: 5000
+    });
+    loading.present();
+    //Conectar na API e salvar os dados, esperando a resposta da api
+    loading.dismiss();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
+
+  }
 
 }
